7e557ac74c648f35ccae664c6c773fe385b8ee4a
Saurabh Naik <buntyspld@gmail.com>
2021-02-02 20:17:57 +0530

    First commit

---
+++ src/main/java/com/saurabh/springbootcustom/SpringBootCustomApplication.java
@@ -0,0 +1,13 @@
+package com.saurabh.springbootcustom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCustomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCustomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saurabh/springbootcustom/SpringBootCustomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saurabh.springbootcustom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCustomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
